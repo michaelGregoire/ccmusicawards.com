a468cd2f78323c31dc738c50ecfc1ffcf6870b3e
Mike Gregoire <mgregoire@nvzion.com>
2021-05-12 18:10:15 -0400

    Add rel='noopener' to ExternalLink

--- src/components/ExternalLink.js
+++ src/components/ExternalLink.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 const ExternalLink = ({ url, ariaLabel }) => {
   return (
-    <a href={url} aria-label={ariaLabel} className="download" target="_blank">
+    <a
+      href={url}
+      aria-label={ariaLabel}
+      className="download"
+      rel="noopener"
+      target="_blank"
+    >
       <i className="fas fa-external-link-square-alt"></i>
     </a>
   );
